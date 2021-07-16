4a10bcba4cdba43d2ffe20b590f84e0b29f707d4
Chandan Jhunjhunwal <chandan.jhunjhunwal@gmail.com>
2021-07-17 03:46:58 +0530

    Using "using" keyword for std namespace

--- main.cpp
+++ main.cpp
@@ -1,11 +1,20 @@
 #include <iostream>
 
 int main() {
-  std::cout << "My world" << std::endl;
-  int x = 5;
-  int y = 10;
-  std::cout << std::endl;
-  std::cout << x + y << " " << x*y;
-  std::cout << std::endl;
-  return 0;
+    using std::cout;
+    using std::endl;
+    cout << "My World.\n";
+    cout << "Here is 5: " << 5 << "\n";
+    cout << "The manipulator endl ";
+    cout << "writes a new line to the screen.";
+    cout << endl;
+    cout << "Here is the sum of 8 and 5:\t";
+    cout << 8 + 5 << endl;
+    cout << "Here is a fraction: \t\t";
+    cout << (float) 5/8 << endl;
+    cout << "And a very very big number:\t";
+    cout << (double) 7000*7000 << endl;
+    cout << "Chandan Jhunjhunwal\n";
+    cout << "A C++ Programmer.";
+    return 0;
 }
